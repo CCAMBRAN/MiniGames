108f08769cea371235f4b4fae1e6e82fab977061
CCAMBRAN <seahawks.cambranis@gmail.com>
2025-12-04 13:24:54 -0600

    commit

--- Models/User.js
+++ Models/User.js
@@ -2,44 +2,43 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
-    username : {
+    username: {
         type: String,
         required: true,
-        unique: true,   
-        minlenghth: 3,
+        unique: true,
+        trim: true,
+        minlength: 3
     },
     email: {
         type: String,
         required: true,
-        lowercase: true,
         unique: true,
+        lowercase: true,
+        trim: true
     },
     password: {
         type: String,
         required: true,
-        minlength: 6,   
+        minlength: 6
     },
-    email: {
-        type: String,
-        required: true,
-        lowercase: true,
-        unique: true,
+    level: {
+        type: Number,
+        default: 1
     },
     experience: {
         type: Number,
-        default: 0,
+        default: 0
     }
-
 }, { timestamps: true });
 
+// Hash password before saving
 userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) {
-        return next();
-        this.password = await bcrypt.hash(this.password, 10);
-        next(); 
-    }
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
 });
 
+// Compare password method
 userSchema.methods.comparePassword = async function (candidatePassword) {
     return await bcrypt.compare(candidatePassword, this.password);
 };
